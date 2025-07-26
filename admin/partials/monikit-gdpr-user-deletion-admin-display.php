ee1ce2fc64dfe58d68a453ae4fff86ef109c5d38
Hasan Burak Taşyürek <hburaktasyurek@gmail.com>
2025-07-26 13:33:23 +0300

    Initial commit of Monikit GDPR User Deletion plugin
    Add all core plugin files, including admin, public, includes, language, and bootstrap files for Monikit App GDPR User Data Deletion. Includes setup for activation/deactivation, internationalization, asset enqueuing, and integration with WordPress. Also adds Composer configuration for development dependencies and removes wpbp.json.

---
+++ admin/partials/monikit-gdpr-user-deletion-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://hbglobal.dev
+ * @since      1.0.0
+ *
+ * @package    Monikit_Gdpr_User_Deletion
+ * @subpackage Monikit_Gdpr_User_Deletion/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ includes/class-monikit-gdpr-user-deletion-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://hbglobal.dev
+ * @since      1.0.0
+ *
+ * @package    Monikit_Gdpr_User_Deletion
+ * @subpackage Monikit_Gdpr_User_Deletion/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Monikit_Gdpr_User_Deletion
+ * @subpackage Monikit_Gdpr_User_Deletion/includes
+ * @author     Hasan Burak Taşyürek <admin@hbglobal.dev>
+ */
+class Monikit_Gdpr_User_Deletion_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
